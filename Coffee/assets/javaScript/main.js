9fd57dc67f05f5f265e9fedd2e823266bc12efb1
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2022-11-09 23:56:14 -0600

    Final__12
    Update to Coffee website.
    Removed Battery project from portfolio.

--- Coffee/assets/javaScript/main.js
+++ Coffee/assets/javaScript/main.js
@@ -88,6 +88,29 @@ window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.products__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) =>{
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) =>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
