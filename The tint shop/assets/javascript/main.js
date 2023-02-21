0d619be801924a2d65849cbbd8cedd98a4e2bd43
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2023-02-20 22:25:55 -0600

    Final__25
    Added The Tint shop website to portfolio

---
+++ The tint shop/assets/javascript/main.js
@@ -0,0 +1,26 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+  
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+                        :header.classList.remove('scroll-header')
+  }
+  
+  window.addEventListener('scroll' , scrollHeader)
+
+
+
+/*==================== SWIPER ====================*/
+var swiperPrev = new Swiper(".prev__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
+
