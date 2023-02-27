02656536bdb0c4e0104abdef4b98ee32542168f8
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2023-02-27 15:10:31 -0600

    FInal__26.3
    Added FAQ page to tint website

--- tint/assets/javascript/main.js
+++ tint/assets/javascript/main.js
@@ -40,7 +40,7 @@ const scrollActive = () =>{
 		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
 			sectionsClass.classList.add('active-link');
 		}else{
-      
+
 			sectionsClass.classList.remove('active-link');
     
 		}                                                    
@@ -48,3 +48,33 @@ const scrollActive = () =>{
 }
 window.addEventListener('scroll', scrollActive)
 
+
+/*=============== QUESTIONS OPEN / CLOSE ===============*/
+
+const accordionItems = document.querySelectorAll('.questions__item')
+
+accordionItems.forEach((item) =>{
+  const accordionHeader = item.querySelector('.questions__header')
+
+  accordionHeader.addEventListener('click', () =>{
+    const openItem = document.querySelector('.accordion-open')
+
+    toggleItem(item)
+
+    if(openItem && openItem!== item){
+      toggleItem(openItem)
+    }
+  })
+})
+
+const toggleItem = (item) =>{
+  const accordionContent = item.querySelector('.questions__content')
+
+  if(item.classList.contains('accordion-open')){
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  }else{
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+}
