28164e26a7840317fed18e2a4d995b35d151f29b
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2023-05-10 20:38:49 -0500

    Final__31.3
    Added the about section to construction website

--- construction/assets/javascript/main.js
+++ construction/assets/javascript/main.js
@@ -22,3 +22,34 @@ const navMenu = document.getElementById('nav-menu')
 navMenu.classList.remove('show-menu')
 }
 navlink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+  
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        :header.classList.remove('blur-header')
+  }
+  
+  window.addEventListener('scroll' , blurHeader)
+
+/*=============== SCROLL SECTIONS ACTIVE ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link');
+		}else{
+			sectionsClass.classList.remove('active-link');
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
