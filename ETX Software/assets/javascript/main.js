bcfd85a3e1a153a38c7576a01a27781057f832c3
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2023-06-05 17:21:48 -0500

    Final__32.7
    Added Client portal login to ETX software website

--- ETX Software/assets/javascript/main.js
+++ ETX Software/assets/javascript/main.js
@@ -91,6 +91,46 @@ sr.reveal(`.home__subtitle, .modal__title, .contact__bkgr, .wrapper-1`,{
   origin: 'left',
 })
 
-sr.reveal(`.socials, .projects__bkgr`,{
+sr.reveal(`.socials, .projects__bkgr, .portal__bkgr`,{
   origin: 'right',
 })
+
+// Define the username-password and HTML file path mappings
+var userCredentials = {
+  admin1: {
+    password: "El mendigo",
+    htmlFile: "assets/dash.html"
+  },
+
+  user2: {
+    password: "pass2",
+    htmlFile: "dash.html"
+  },
+
+  user3: {
+      password: "pass3",
+      htmlFile: "dash.html"
+    },
+  // Add more username-password mappings as needed
+};
+
+function authenticateUser(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Retrieve username and password from the form
+  var username = document.getElementById("id").value;
+  var password = document.getElementById("password").value;
+
+  // Check if the entered credentials exist in the userCredentials object
+  if (username in userCredentials && password === userCredentials[username].password) {
+    alert("Authentication successful! Redirecting to the dashboard...");
+
+    // Redirect to the corresponding HTML file
+    window.location.href = userCredentials[username].htmlFile;
+  } else {
+    alert("Invalid username or password. Please try again.");
+  }
+}
+
+var form = document.getElementById("login-form");
+form.addEventListener("submit", authenticateUser);
