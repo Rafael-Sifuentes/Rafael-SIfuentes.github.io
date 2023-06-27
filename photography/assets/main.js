c8780c84a5119afa1122990a945e6c5afcae0b47
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2023-06-27 01:28:53 -0500

    Final__32.9
    Added photography website

---
+++ photography/assets/main.js
@@ -0,0 +1,35 @@
+
+
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+if(navToggle){
+navToggle.addEventListener('click', () =>{
+navMenu.classList.add('show-menu')
+})
+}
+
+if(navClose){
+navClose.addEventListener('click', () =>{
+navMenu.classList.remove('show-menu')
+})
+}
+
+const navlink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show-menu')
+}
+navlink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+  
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        :header.classList.remove('blur-header')
+  }
+  
+  window.addEventListener('scroll' , blurHeader)
